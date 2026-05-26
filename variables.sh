4397244ce4eaad2fb64f80c5fb5c84e7f2632405
Sumanth1 <challa.sumanthreddy86@gmail.com>
2026-05-26 11:04:42 +0530

    Script started

--- variables.sh
+++ variables.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo "All variables passed to script: $#"
+echo "Number of variables passed:$@"
+echo " First variable is:$1"
+echo "Script name is: $0"
+echo "who is running this: $USER"
+echo "Which directory: $PWD"
+echo "Home directory: $HOME"
+echo "PID of the current script: $$"
+sleep 5 &
+echo "PID of the background command running just now: $!"
+wait $!
+echo "Line number: $LINENO"
+echo "Script executed in $SECONDS seconds"
+echo "Random number: $RANDOM"
