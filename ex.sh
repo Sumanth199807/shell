dd927df8c947e0737be76773e5c610af5e097fd5
Sumanth1 <challa.sumanthreddy86@gmail.com>
2026-05-26 10:56:56 +0530

    Script started

--- ex.sh
+++ ex.sh
@@ -1,3 +1,14 @@
 #!bin/bash
 
-echo "Good moring"
+
+START_TIME=$(date +%s)
+sleep 10
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+
+echo "Script executed in $TOTAL_TIME seconds"
+
+
